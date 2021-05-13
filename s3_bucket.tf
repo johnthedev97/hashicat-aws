0c7d1759d848f526d62c3d785ab9796bcafb2c46
johnthdev <johnthdev97@gmail.com>
2021-05-13 21:49:30 +0000

    update to latest version

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/john-chip-training/s3-bucket/aws"
-  version = "1.5.0"
+  version = "2.1.0"
   bucket_prefix = var.prefix
   # insert required variables here
 }
